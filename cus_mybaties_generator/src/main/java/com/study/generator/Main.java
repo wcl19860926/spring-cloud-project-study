817b70686f9a35cf577d7129d46b82aba430e60e
wcl19860926 <694216530@qq.com>
2020-06-14 18:22:57 +0800

    add

---
+++ cus_mybaties_generator/src/main/java/com/study/generator/Main.java
@@ -0,0 +1,8 @@
+package com.study.generator;
+
+public class Main {
+
+    public  static  void main(String[] args){
+
+    }
+}
---
+++ cus_mybaties_generator/src/main/java/com/study/generator/gen/CusAutoGenerator.java
@@ -0,0 +1,19 @@
+package com.study.generator.gen;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
+import com.baomidou.mybatisplus.generator.engine.AbstractTemplateEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CusAutoGenerator {
+
+    private static final Logger logger = LoggerFactory.getLogger(com.baomidou.mybatisplus.generator.AutoGenerator.class);
+
+    private TemplateConfig template;
+    private AbstractTemplateEngine templateEngine;
+
+
+}
